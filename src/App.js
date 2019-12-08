4308709802f2da58de8e44eb28490ee4c53300d3
NicolaAstra <mcsnow78@gmail.com>
2019-12-09 05:12:22 +0700

    set up react app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div>Hello Word</div>;
+}
+
+export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content"></div>
+      </div>
+    </div>
+  </div>
+);
