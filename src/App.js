c7a47cc3e3fc02f1c08add73494f9848e6fdbbe3
NicolaAstra <mcsnow78@gmail.com>
2019-12-10 06:28:55 +0700

    setup commponent and sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./homepage.component";
 
 function App() {
-  return <div>Hello Word</div>;
+  return (
+    <div className="app">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
