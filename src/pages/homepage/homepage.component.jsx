f867ba90ef6344e9871ec60263198892988d3dba
NicolaAstra <mcsnow78@gmail.com>
2019-12-11 19:35:32 +0700

    setup react-router-dom and menu-item to HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,7 @@ import Directory from "../../components/directory/directory.component";
 
 import "./homepage.styles.scss";
 
-const HomePage = () => (
+const HomePage = ({ history }) => (
   <div className="homepage">
     <h1>Welcome to my Homepage</h1>
     <Directory />
