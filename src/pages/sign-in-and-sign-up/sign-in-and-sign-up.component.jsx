bad348b923b261ba2a809e9fb35a07c70631f633
NicolaAstra <mcsnow78@gmail.com>
2019-12-24 15:01:38 +0700

    finish sign up and sign in with firebase

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../components/sing-in/sing-in.component";
+import SignUp from "../../components/sign-up/sing-up.component";
 
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
   <div className="sing-in-and-sing-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
