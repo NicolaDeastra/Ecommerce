f376968e21fc8e461dcd075ce61a1b2bafb3a1da
NicolaAstra <mcsnow78@gmail.com>
2020-01-02 19:18:52 +0700

    updated reduw with reselect

--- src/App.js
+++ src/App.js
@@ -27,9 +27,8 @@ class App extends React.Component {
             ...snapShot.data()
           });
         });
-      } else {
-        setCurrentUser(userAuth);
       }
+      setCurrentUser(userAuth);
     });
   }
 
