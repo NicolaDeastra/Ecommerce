ebae722dd971b7423dd5d7a228af3eeb61f904ac
NicolaDonoastro <nicolaardiansya@gmail.com>
2020-02-01 18:31:53 +0700

    create a WithSpinner HOC as well as modified shop to initialize the call for data.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
-
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
-import "./collections-overview.styles.scss";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
+
+import { CollectionsOverviewContainer } from "./collections-overview.styles";
 
 const CollectionsOverview = ({ collections }) => (
-  <div className="collections-overview">
+  <CollectionsOverviewContainer className="collections-overview">
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
-  </div>
+  </CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
